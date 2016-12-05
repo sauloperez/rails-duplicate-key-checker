3bc320f4255e066cc529e52fce7c4e1f051632ee
Pau Perez <saulopefa@gmail.com>
2016-12-05 10:26:52 +0100

    First commit

---
+++ lib/rails/duplicate/key/checker.rb
@@ -0,0 +1,11 @@
+require "rails/duplicate/key/checker/version"
+
+module Rails
+  module Duplicate
+    module Key
+      module Checker
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/rails/duplicate/key/checker/version.rb
@@ -0,0 +1,9 @@
+module Rails
+  module Duplicate
+    module Key
+      module Checker
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
